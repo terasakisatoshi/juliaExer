79702134493b5260129700d523607c1ddadfbd70
Satoshi Terasaki <terasakisatoshi.math@gmail.com>
2019-12-28 22:43:18 +0900

    update

--- videoIO/demo.jl
+++ videoIO/demo.jl
@@ -6,19 +6,30 @@ The original implementation is taken from VideoIO.jl(see __init__ function)
 using Makie
 using VideoIO
 
+function center_crop(img)
+    imW, imH = size(img)
+    sz = min(imH, imW)
+    y_offset = round(Int,(imH - sz) / 2.)
+    x_offset = round(Int,(imW - sz) / 2.)
+    y_slice = range(1+y_offset, stop=y_offset+sz)
+    x_slice = range(1+x_offset, stop=x_offset+sz)
+    cropped = img[x_slice, y_slice]
+    cropped
+end
+
 function play(f; flipx = false, flipy = false, pixelaspectratio = nothing)
     if pixelaspectratio ≡ nothing # if user did not specify the aspect ratio we'll try to use the one stored in the video file
         pixelaspectratio = VideoIO.aspect_ratio(f)
     end
-    h = f.height
-    w = round(typeof(h), f.width * pixelaspectratio) # has to be an integer
-    scene = Makie.Scene(resolution = (w, h))
+    sz=min(f.height, f.width)
+    scene = Makie.Scene(resolution = (sz,sz))
     buf = read(f)
+    disp=center_crop(buf)
     makieimg = Makie.image!(
         scene,
-        1:h,
-        1:w,
-        buf,
+        1:sz,
+        1:sz,
+        disp,
         show_axis = false,
         scale_plot = false,
     )[end]
@@ -32,14 +43,15 @@ function play(f; flipx = false, flipy = false, pixelaspectratio = nothing)
     display(scene)
     while !eof(f) && isopen(scene)
         read!(f, buf)
-        makieimg[3] = buf
+        disp=center_crop(buf)
+        makieimg[3] = disp
         sleep(1 / f.framerate)
     end
 
 end
 
 
-function startdemo(pixelaspectratio = nothing)
+function begindemo(pixelaspectratio = nothing)
     camera = opencamera()
     play(camera, flipx = true, pixelaspectratio = pixelaspectratio)
     close(camera)
