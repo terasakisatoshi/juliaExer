44bde3bdf3cce37d26a10e7a08d12bcec4a60094
Satoshi Terasaki <terasakisatoshi.math@gmail.com>
2019-12-29 20:08:41 +0900

    update

--- weaveExample/linearRegression/generate.jl
+++ weaveExample/linearRegression/generate.jl
@@ -1,3 +1,9 @@
 using Weave
 
-weave("docs.jmd",out_path=:pwd,doctype="md2html")
+# weave("docs.jmd",out_path=:pwd,doctype="md2html")
+weave(
+    "docs.jmd",
+    out_path=:pwd,
+    doctype="md2pdf",
+    template="julia_tex.tpl",
+)
